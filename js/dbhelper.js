74cd82bf25659f3e061d155402ca6d4af1f8b92d
Douglas Lopes <douglas.lopes@odonto.cromg.org.br>
2018-07-12 12:16:08 -0300

    fix: fix english and improving readme

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant (restaurant) {
-    return (`/img/${restaurant.photograph}`)
+    return (`https://github.com/dwolopes/Restaurant-Reviews/tree/master/img/${restaurant.photograph}`)
   }
 
   /**
