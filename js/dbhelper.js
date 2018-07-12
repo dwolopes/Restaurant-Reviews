a743351144b6f0d0ba8ea5c4ba27918b35af73ea
Douglas Lopes <douglas.lopes@odonto.cromg.org.br>
2018-07-12 12:18:57 -0300

    fix: fixing accessibility functions on restaurant detail page

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,9 +7,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL () {
-    // const port = 8000 // Change this to your server port
-    // return `http://localhost:${port}/data/restaurants.json`
-    return `https://github.com/dwolopes/Restaurant-Reviews/blob/master/data/restaurants.json`
+    const port = 8000 // Change this to your server port
+    return `http://localhost:${port}/data/restaurants.json`
   }
 
   /**
@@ -150,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant (restaurant) {
-    return (`https://github.com/dwolopes/Restaurant-Reviews/tree/master/img/${restaurant.photograph}`)
+    return (`/img/${restaurant.photograph}`)
   }
 
   /**
